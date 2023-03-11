d22c42559c503d6aec3fb71eeeb8e6e68c464188
Mark P. Earl <mpe12@uakron.edu>
2023-03-11 10:48:36 -0500

    Add implementation

--- HNode.cpp
+++ HNode.cpp
@@ -3,13 +3,34 @@
 
 #include "HNode.hpp"
 
-// constructor for leaf nodes
-HNode::HNode(const char& s, const int& t) {
+// helper functions
 
+// @param root The root of the heap
+// @retval The total number of nodes in the tree/subtree from root
+int totalNodes(HNode* root)
+{
+    if (root == nullptr)
+        return 0;
+
+    int l = totalNodes(root->left);
+    int r = totalNodes(root->right);
+
+    return 1 + l + r;
+}
+
+// constructor for leaf nodes
+HNode::HNode(const char& character, const int& frequency) {
+
+    // For leaf nodes, the value member is the character...
+    value = character;
+    // ...and the weight member the frequency count.
+    weight = frequency;
 }
 
 // constructor for internal nodes
 HNode::HNode(HNode* l, HNode* r) {
 
+    // For internal nodes value is null (already set)
+    // The weight is the combined count of the nodes in the subtrees.
+    weight = totalNodes(this);
 }
-
