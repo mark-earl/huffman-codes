6655e916b1b9832960e69a5c749af61fa4397a07
Mark P. Earl <mpe12@uakron.edu>
2023-03-10 22:37:25 -0500

    Add implementation for enqueue

--- Heap.cpp
+++ Heap.cpp
@@ -2,14 +2,27 @@
     @file Heap.cpp
 
     Implementation file for the Heap class
+
+    @Note This is the implementation of a min-heap
 */
 
 #include "Heap.hpp"
 
 // add an element to the tree
-void Heap::enqueue( HNode*) {
+void Heap::enqueue(HNode* newElement) {
 
-    // @TODO Implement enqueue
+    // Add a new leaf
+    tree.push_back(0);
+    int index = tree.size() - 1;
+
+    // Demote parents that are larger than the new element
+    while ((index > 1) && (tree[index / 2]->value > newElement->value)) {
+        tree[index] = tree[index / 2];
+        index = index /  2;
+    }
+
+    // Store the new element into the vacant slot
+    tree[index] = newElement;
 }
 
 // remove the smallest element
