972e797a8e0883951483259062d45d035ac24bde
Mark P. Earl <mpe12@uakron.edu>
2023-03-10 22:27:12 -0500

    Add empty function definitions

---
+++ Heap.cpp
@@ -0,0 +1,36 @@
+/*
+    @file Heap.cpp
+
+    Implementation file for the Heap class
+*/
+
+#include "Heap.hpp"
+
+// add an element to the tree
+void Heap::enqueue( HNode*) {
+
+    // @TODO Implement enqueue
+}
+
+// remove the smallest element
+HNode* Heap::dequeue() {
+
+    // @TODO Implement dequeue
+}
+
+// fix the heap from a specific index up
+void Heap::fix_up(const int&) {
+
+    // @TODO Implement fix_up
+}
+
+// fix the tree after replacing the smallest element
+void Heap::fix_down(const int&) {
+
+    // @TODO Implement fix_down
+}
+
+void Heap::clear() {
+
+    // @TODO Implement clear
+}
