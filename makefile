372adc47fa9dbef086a3a7695554d1d206f335c0
Mark P. Earl <mpe12@uakron.edu>
2023-03-11 19:53:30 -0500

    Clean up

--- makefile
+++ makefile
@@ -4,18 +4,20 @@ FLAGS = -std=c++11
 all: huffman.out clean
 
 Heap.o: Heap.cpp
-	    ${CC} ${FLAGS} -c Heap.cpp
+	    @${CC} ${FLAGS} -c Heap.cpp
 
 HNode.o: HNode.cpp
-	    ${CC} ${FLAGS} -c HNode.cpp
+	    @${CC} ${FLAGS} -c HNode.cpp
 
 Huffman.o: Huffman.cpp
-		${CC} ${FLAGS} -c Huffman.cpp
+		@${CC} ${FLAGS} -c Huffman.cpp
 
 huffman.out: Heap.o HNode.o Huffman.o test_huffman.cpp
-		${CC} ${FLAGS} test_huffman.cpp *.o -o huffman.out
+		@echo Building executable: huffman.out
+		@${CC} ${FLAGS} test_huffman.cpp *.o -o huffman.out
 
 clean:
-	rm -f *.o
-	rm -f *~
-	rm -f \#*
+	@echo Cleaning up...
+	@rm -f *.o
+	@rm -f *~
+	@rm -f \#*
--- test_huffman.cpp
+++ test_huffman.cpp
@@ -3,10 +3,14 @@
 
 #include <iostream>
 #include <cassert>
+#include <random>
+#include <algorithm>
 #include "Huffman.hpp"
 
 int main() {
     Huffman h;
+
+    std::cout << "Huffman Code Test (1/2)\n\n";
 
     std::cout << "Input: MISSISSIPPI\n\n";
     Encoded e = h.encode("MISSISSIPPI");
@@ -21,8 +25,7 @@ int main() {
     assert(e.serialized_tree == "*I//**M//P//S//");
     std::cout << "\tPASSED\n";
 
-    std::cout << "All Tests Passed\n\n";
-
+    std::cout << "\nHuffman Code Test (2/2)\n\n";
 
     std::cout << "Input: A man a plan, a canal, Panama!\n\n";
     Encoded a = h.encode("A man a plan, a canal, Panama!");
@@ -31,12 +34,14 @@ int main() {
 
     //@TODO implement theses tests
     std::cout << "Testing Encoded String...";
-    assert(a.encoded_string == "");
+    assert(a.encoded_string == "010100010111110000110001101101011100011100110001001110011101001110001011111001110111101100");
     std::cout << "\tPASSED\n";
 
     std::cout << "Testing Serialized Tree...";
-    assert(a.serialized_tree == "");
+    assert(a.serialized_tree == "** //**c//*A//P//**!//p//,//**n//*l//m//a//");
     std::cout << "\tPASSED\n";
 
-    std::cout << "All Tests Passed\n\n";
+    std::cout << "\nAll Tests Passed\n\n";
+
+    return 0;
 }
